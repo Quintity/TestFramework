93c99e2812a108f56a90b1ba8e98313fb0189984
James Mothershead <jmothershead@quintity.com>
2019-01-02 13:53:06 -0800

    Add project and item templates and vsix project.

---
+++ Templates/ItemTemplates/BasicTestClass/TestClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QTF = Quintity.TestFramework.Core;
+
+namespace $rootnamespace$
+{
+    [QTF.TestClass]
+    public class $safeitemrootname$ : QTF.TestClass
+    {
+        #region Data members
+
+        #endregion
+
+        #region Constructors
+
+        public $safeitemname$()
+        { }
+
+        #endregion
+
+        #region Test methods
+
+        [QTF.TestMethod]
+        public QTF.TestResult MyTestMethod(
+            [QTF.TestParameter("String parameter", "Enter string parameter", "Default value")]
+            string param1,
+            [QTF.TestParameter("Int parameter", "Enter integer parameter", 0)]
+            int param2,
+            [QTF.TestParameter("Boolean parameter", "Enter boolean parameter", true)]
+            bool param3)
+        {
+            try
+            {
+                Setup();
+
+                QTF.TestTrace.Trace("This is a sample TestTrace message.");
+
+                QTF.TestAssert.IsTrue(param3, "This is a sample assertion.");
+
+                QTF.TestCheck.IsTrue("Test of param3", param3, "This is a sample TestCheck.");
+
+                TestMessage += "This is the returned test message.";
+                TestResult = QTF.TestResult.Pass;
+            }
+            catch (QTF.TestAssertFailedException e)
+            {
+                TestMessage += e.ToString();
+                TestResult = QTF.TestResult.Fail;
+            }
+            catch (Exception e)
+            {
+                TestMessage += e.ToString();
+                TestResult = QTF.TestResult.Error;
+            }
+            finally
+            {
+                Teardown();
+            }
+
+            return TestResult;
+        }
+
+        #endregion
+
+        #region Private methods
+
+        #endregion
+
+        #region Protected methods
+
+        protected override void Setup()
+        {
+            base.Setup();
+        }
+
+        protected override void Teardown()
+        {
+            base.Teardown();
+        }
+
+        #endregion
+    }
+}
---
+++ Templates/ItemTemplates/TestListenerClass/TestListener.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QTF = Quintity.TestFramework.Core;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : QTF.ITestExecutionListener
+    {
+        public void TestExecutionBegin(Dictionary<string, object> listenerProperties, QTF.PreExecutionEventArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestSuiteExecutionBegin(QTF.TestSuite testSuite)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestPreprocessorExecutionBegin(QTF.TestSuite testSuite, QTF.TestProcessBeginEventArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestPreprocessorExecutionComplete(QTF.TestSuite testSuite, QTF.TestProcessCompleteEventArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestCaseExecutionBegin(QTF.TestCase testCase)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestStepExecutionBegin(QTF.TestStep testStep)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestTrace(string traceMessage)
+        {
+            // Enter runtime event handler code here
+        }
+        
+        public void TestCheck(QTF.TestCheck testCheck)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestStepExecutionComplete(QTF.TestStep testStep, QTF.TestExecutionCompleteEventArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestCaseExecutionComplete(QTF.TestCase testCase, QTF.TestExecutionCompleteEventArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestPostprocessorExecutionBegin(QTF.TestSuite testSuite, QTF.TestProcessBeginEventArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestPostprocessorExecutionComplete(QTF.TestSuite testSuite, QTF.TestProcessCompleteEventArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestSuiteExecutionComplete(QTF.TestSuite testSuite, QTF.TestExecutionCompleteEventArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestExecutionComplete(QTF.PostExecutionEventArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void TestExecutionStopRequest(QTF.TestScriptObject testScriptObject, QTF.TestExecutionStopRequestArgs eventArgs)
+        {
+            // Enter runtime event handler code here
+        }
+
+        public void Dispose()
+        {
+            // Enter runtime event handler code here
+        }
+    }
+}
