e46a082007bf80ff5d0ef58e6bbcdadfcc59ec53
James Mothershead <jmothershead@quintity.com>
2019-01-31 16:12:45 -0800

    Added/updated msi

--- Source/Quintity.TestFramework.TestListenersService/TestListenersService.Setup/Program.cs
+++ Source/Quintity.TestFramework.TestListenersService/TestListenersService.Setup/Program.cs
@@ -9,17 +9,49 @@ namespace TestListenersService.Setup
 {
     class Program
     {
+#if DEBUG
+        static private string binSource = @"..\Quintity.TestFramework.TestListenersService.Host\bin\Debug\{0}";
+        static private string build = "Debug";
+#else
+        static private string binSource = @"..\Quintity.TestFramework.TestListenersService.Host\bin\Release\{0}");
+        static private string build = "Release";
+#endif
+
         static void Main()
         {
-            var project = new Project("MyProduct",
-                              new Dir(@"%ProgramFiles%\My Company\My Product",
-                                  new File("Program.cs")));
+            File service = null;
 
-            project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
-            //project.SourceBaseDir = "<input dir path>";
-            //project.OutDir = "<output dir path>";
+            Project project = new Project("Quintity.TestFramework.TestListenersService",
+                new Dir(@"C:\Quintity.TestFramework.TestListenersService",
+                    service = new File(string.Format(binSource, "Quintity.TestFramework.TestListenersService.Host.exe")),
 
-            project.BuildMsi();
+                    new Files($@"..\Quintity.TestFramework.TestListenersService.Host\bin\{build}\*.dll"),
+                    new Files($@"..\Quintity.TestFramework.TestListenersService.Host\bin\{build}\*.config"),
+
+                    new Dir("%Desktop%",
+                        new ExeFileShortcut("Quintity TestLIsteners Service", "[INSTALLDIR]Quintity.TestFramework.TestListenersService.Host.exe", "")),
+
+                    new ExeFileShortcut("Uninstall TestListenersService", "[System64Folder]msiexec.exe", "/x [ProductCode]"))
+            );
+
+            service.ServiceInstaller = new ServiceInstaller
+            {
+                Name = "Quintity TestListenersService",
+                Description = "Quintity runtime listener test management service.",
+
+                DelayedAutoStart = false,
+                StartOn = SvcEvent.Install,
+                StopOn = SvcEvent.InstallUninstall_Wait,
+                RemoveOn = SvcEvent.Uninstall_Wait,
+                Arguments = "/S"
+            };
+
+            project.OutDir = $@".\bin\{build}\";
+            project.ControlPanelInfo.Manufacturer = "Quintity, LLC";
+            project.ControlPanelInfo.InstallLocation = "[INSTALLDIR]";
+            project.GUID = new Guid("96F03431-CD66-4A3E-958D-E71FE7662BD3");
+            project.UI = WUI.WixUI_ProgressOnly;
+            Compiler.BuildMsi(project);
         }
     }
 }
