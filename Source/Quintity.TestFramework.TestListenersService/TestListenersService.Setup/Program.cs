f64ffb87b2cfd501461747379a467fed2056203a
James Mothershead <jmothershead@quintity.com>
2019-01-31 16:11:51 -0800

    Added installer and *.msis'

---
+++ Source/Quintity.TestFramework.TestListenersService/TestListenersService.Setup/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WixSharp;
+
+// DON'T FORGET to update NuGet package "WixSharp".
+// NuGet console: Update-Package WixSharp
+// NuGet Manager UI: updates tab
+
+namespace TestListenersService.Setup
+{
+    class Program
+    {
+        static void Main()
+        {
+            var project = new Project("MyProduct",
+                              new Dir(@"%ProgramFiles%\My Company\My Product",
+                                  new File("Program.cs")));
+
+            project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+            //project.SourceBaseDir = "<input dir path>";
+            //project.OutDir = "<output dir path>";
+
+            project.BuildMsi();
+        }
+    }
+}
